d57ba7be0c90d6dd74b46baf551ff3fd9636a463
Satyam Kashyap <satyamsudo@gmail.com>
2023-07-11 14:50:21 +0530

    beech ka commit

--- reimbursementGUI.py
+++ reimbursementGUI.py
@@ -8,6 +8,13 @@ import threading
 from tkinter.ttk import Progressbar
 
 selected_files = []  # Global list to store the selected files
+stop_program = False  # Global flag to indicate if the program should stop
+
+def emptyFun():
+    print('Empty')
+
+threadMain = threading.Thread(target=emptyFun)
+
 
 def select_files():
     global selected_files  # Use the global list to store the selected files
@@ -24,13 +31,11 @@ def select_files():
         run_button.config(state=tk.DISABLED)  # Disable the "Run Program" button
 
 def run_program():
+    global stop_program  # Use the global flag to check if the program should stop
+    global threadMain
     progress_label.config(text="Running...")
-    progress_bar.start(10)
-
-    # progress_bar["maximum"] = 50  # Set the maximum value for the progress bar
-    # progress_bar["value"] = 0  # Reset the progress bar value
-    # progress_bar.step(5)
-    update_gui()  # Update the GUI
+    progress_bar.config(mode="indeterminate")  # Set the progress bar mode to indeterminate
+    progress_bar.start()  # Start the indeterminate progress bar
 
     def program_thread():
         print(*selected_files, sep="\n")
@@ -42,15 +47,15 @@ def run_program():
         for file in selected_files:
             results_normal.append(reader.readtext(str(file)))
 
-        # progress_bar.step(5)  # Increment progress by 5
-        update_gui()  # Update the GUI
-
         totalAmounts = []
 
         def return_back_last_rupee(list_of_files):
             matches = []
 
             for index, different_file in enumerate(list_of_files):
+                if stop_program:  # Check if the program should stop
+                    break
+
                 for match_contents in different_file:
                     if 'र' in match_contents[1]:
                         matches.append(match_contents[1])
@@ -61,9 +66,7 @@ def run_program():
 
                 matches = []
 
-                if index % 10 == 0:  # Update progress bar at every 10th iteration
-                    # progress_bar.step(10 / len(list_of_files))  # Increment progress
-                    update_gui()  # Update the GUI
+                update_gui()  # Update the GUI
 
             update_gui()  # Update the GUI
 
@@ -85,6 +88,9 @@ def run_program():
         converted_to_english_numeral_amounts = []
 
         for numbers in totalAmounts:
+            if stop_program:  # Check if the program should stop
+                break
+
             for hindi_num, english_num in hindi_to_english.items():
                 numbers = numbers.replace(hindi_num, english_num)
 
@@ -94,56 +100,66 @@ def run_program():
 
         total = 0.0
         for amount in totalAmounts:
+            if stop_program:  # Check if the program should stop
+                break
+
             num_without_rupee = amount[1:]  # Remove the first character (rupee symbol)
             total += float(num_without_rupee)
 
-        with open('Reimbursement.txt', 'w', encoding="utf-8") as f:
-            f.write('Total Reimbursement amount in chronological order: ')
-            f.write('\n')
-            f.write('\n')
-            for line in totalAmounts:
-                f.write(line)
+        if not stop_program:  # Only proceed if the program should not stop
+            with open('Reimbursement.txt', 'w', encoding="utf-8") as f:
+                f.write('Total Reimbursement amount in chronological order: ')
                 f.write('\n')
+                f.write('\n')
+                for line in totalAmounts:
+                    f.write(line)
+                    f.write('\n')
 
-            f.write('\n')
-            f.write('Total Amount: ')
-            f.write(str(total))
+                f.write('\n')
+                f.write('Total Amount: ')
+                f.write(str(total))
 
-        # progress_bar.step(2.5)  # Increment progress by 2.5
-        update_gui()  # Update the GUI
+            current_date = datetime.date.today()
+            previous_month = current_date.month - 1 if current_date.month > 1 else 12
 
-        current_date = datetime.date.today()
-        previous_month = current_date.month - 1 if current_date.month > 1 else 12
+            month_name = calendar.month_name[previous_month]
 
-        month_name = calendar.month_name[previous_month]
+            images = []
+            for file in selected_files:
+                images.append(Image.open(file))
 
-        images = []
-        for file in selected_files:
-            images.append(Image.open(file))
+            pdf_path = "Reimbursement_" + month_name + ".pdf"
 
-        pdf_path = "Reimbursement_" + month_name + ".pdf"
+            images[0].save(
+                pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
+            )
 
-        images[0].save(
-            pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
-        )
-
-        # progress_bar.step(2.5)  # Increment progress by 2.5
-        update_gui()  # Update the GUI
-
-        progress_bar.stop()  # Stop the progress bar
+        progress_bar.stop()  # Stop the indeterminate progress bar
         progress_label.config(text="Completed!")
 
-    thread = threading.Thread(target=program_thread)
-    thread.start()
+    threadMain = threading.Thread(target=program_thread)
+    threadMain.start()
+
+def stop_program():
+    global stop_program  # Use the global flag to indicate if the program should stop
+    stop_program = True
+    resetToStartup()
+
 
 def update_gui():
     window.update()
     window.update_idletasks()
 
+def resetToStartup():
+    progress_label.config(text="Waiting for selection of files")
+    select_files = []
+    run_button.config(state=tk.DISABLED)  # Disable the "Run Program" button
+    progress_bar.stop()
+
 # Create the main window
 window = tk.Tk()
 window.title("File Selection")
-window.geometry("300x200")
+window.geometry("300x250")
 
 # File selection button
 select_button = tk.Button(window, text="Select Files", command=select_files)
@@ -158,6 +174,10 @@ progress_bar.pack(pady=10)
 # Run program button
 run_button = tk.Button(window, text="Run Program", command=run_program, state=tk.DISABLED)
 run_button.pack(pady=10)
+
+# Stop program button
+stop_button = tk.Button(window, text="Stop Program", command=stop_program)
+stop_button.pack(pady=5)
 
 # Start the GUI event loop
 window.mainloop()
